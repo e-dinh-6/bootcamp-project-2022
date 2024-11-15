a363ec372209e83e48268fbbc1cfc653772a49fc
Emi <emidinh6@gmail.com>
2024-11-14 19:01:27 -0800

    still doing blog preview

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import BlogPreview from "@/components/blogPreview";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",

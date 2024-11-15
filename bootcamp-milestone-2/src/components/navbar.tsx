84466fee5352ca2108d5d928d10c6ee09f2e85e6
Emi <emidinh6@gmail.com>
2024-11-14 18:32:22 -0800

    added navbar component, create resume page.tsx

---
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    // replace everything in between the <header> & <header /> tags
+    // with your navbar code from your earlier milestones
+    // NOTE: we use "class" in HTML but React is quirky so we have to
+    // change all the "class" to "className"
+    <header className={style.navbar}>
+      <h1 class="logo">
+        <a href="index.html">Emi Dinh</a>
+      </h1>
+      <nav class="navbar">
+        <ul class="nav-list">
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="blog.html">Blog</a>
+          </li>
+          <li>
+            <a href="portfolio.html">Portfolio</a>
+          </li>
+          <li>
+            <a href="resume.html">Resume</a>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

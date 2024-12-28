b7a30c94292da61785f9f1c216bf42e292a46395
Emi <emidinh6@gmail.com>
2024-12-28 02:00:03 -0800

    finished milestone 3A

--- bootcamp-milestone-3/src/app/portfolio/page.tsx
+++ bootcamp-milestone-3/src/app/portfolio/page.tsx
@@ -1,51 +1,45 @@
+import PortfolioComp from "@/components/portfolio";
 import React from "react";
 import styles from "./portfolio.module.css";
+import connectDB from "@/database/db";
+import Portfolio from "@/database/portfolioSchema";
 
-export default function Portfolio() {
-  return (
-    <div>
+export default async function Portfolios() {
+  const list_of_port = await getPortfolio();
+  if (!list_of_port) {
+    return <div>Portfolio Not Found</div>;
+  } else {
+    return (
       <div className={styles.project}>
-        <img
-          src="images/website_sc.png"
-          alt="description"
-          className="project-image"
-        />
-        <div className={styles.projectBox}>
-          <p className={styles.projectName}>Personal Website</p>
-          <p className={styles.projectDetails}>
-            With the Hack4Impact starter pack as my guide, I developed a
-            five-page website that demonstrates my skills in web development and
-            Git source control. This platform not only showcases my progress in
-            website building but also serves as a hub for my projects.
-          </p>
+        <section className={styles.intro}>
+          <h1 className={styles.pageTitle}>My Portfolio</h1>
+        </section>
+
+        <div className={styles.blogContainer}>
+          {list_of_port.map((port, index) => (
+            <PortfolioComp
+              key={index}
+              projectName={port.projectName}
+              image={port.image}
+              imageAlt={port.imageAlt}
+              projectDetails={port.projectDetails}
+            />
+          ))}
         </div>
       </div>
+    );
+  }
+}
 
-      <div className={styles.project}>
-        <a href="https://users.csc.calpoly.edu/~ddang13/Gastronomy%20Galaxy/">
-          <img
-            src="images/gastronomyGalaxy.png"
-            alt="description"
-            className="project-image"
-          />
-        </a>
-        <div className={styles.projectBox}>
-          <p className={styles.projectName}>Gastronomy Galaxy</p>
-          <p className={styles.projectDetails}>
-            "Galaxy Gastronomy" is a 2D smoothie simulator that invites players
-            to blend delicious smoothies for quirky alien customers in a vibrant
-            space setting. Developed collaboratively by Emi Dinh, Danny Dang,
-            and Kasey Liu, the game features three engaging levels with
-            randomized orders, emphasizing a relaxing single-player experience.
-            Inspired by titles like "Overcooked" and "Papa’s Freezeria," it
-            combines elements of time management and customer engagement while
-            focusing on calm gameplay. Despite facing challenges with game
-            mechanics and controls, we successfully implemented core features
-            such as blending, cleaning, and distributing smoothies to customers
-            .
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+async function getPortfolio() {
+  await connectDB(); // function from db.ts before
+
+  try {
+    // query for all blogs and sort by date
+    const portfolios = await Portfolio.find().orFail();
+    // send a response as the blogs as the message
+    return portfolios;
+  } catch (err) {
+    return null;
+  }
 }
